80506232262cf03ca85f38f459184521f70224be
ImanNazhat <emannzhat@gmail.com>
2024-03-27 09:50:22 +0000

    le code qui affichier le tableux

--- controllers/AdminController.php
+++ controllers/AdminController.php
@@ -4,6 +4,14 @@ class AdminController extends AbstractController
                 {
                     public function AdminMenu() : void
                     {
+                        $menu = new MenuManager;
+                        
+                        $menus = $menu->findAll();
+                        //dump ($menus);
+                        
+                       $this->render("admin/admin-menu.html.twig", [
+                           "menus" => $menus
+                       ]);
                        
                     }
                      
--- controllers/MenuController.php
+++ controllers/MenuController.php
@@ -4,10 +4,21 @@ class MenuController extends AbstractController
                 {
                     public function Menu() : void
                     {
-                       
+                       echo "méthode Menu";
                     }
                     public function MenuVegetarian() : void
                     {
+                        //pour essayer que ma function marche bien
+                    //     $menuManager = new MenuManager();
+                    //   $vegetarians = $menuManager->findAll();
+                    //   dump($vegetarians);
                        
+                    //   $menuManager = new MenuManager();
+                    //   $vegetarians = $menuManager->findOne(3); //ici il faut que j'ai ecrir le numero de l'item que je le veux
+                    //   dump($vegetarians);
+                       
+                    //   $menuManager = new MenuManager();
+                    //   $vegetarians = $menuManager->findVegetarian();
+                    //   dump($vegetarians);
                     }
                 }
